a70fb134236332caa26b00f93186b32448ef38a2
Sperce <octave@debrich.net>
2019-03-27 17:41:45 +0100

    Puppy!
    Ajout du puppy et correection d'un élément de puppet master

--- Assets/Puppet scripts/Puppet.cs
+++ Assets/Puppet scripts/Puppet.cs
@@ -6,50 +6,27 @@ using Leap;
 public class Puppet : MonoBehaviour {
     public GameObject Corps;
     Controller m_leapController;
+    GameObject weird;
+    GameObject LeftPalm;
+    GameObject RightPalm;
+
     // Use this for initialization
     void Start () {
         m_leapController = new Controller();
+        weird = GameObject.FindGameObjectWithTag("Weird");
     }
 
     void weirdWays()
     {
-        Frame frame = m_leapController.Frame();
+        RightPalm = GameObject.FindGameObjectWithTag("Right palm");
+        LeftPalm = GameObject.FindGameObjectWithTag("Left palm");
 
-        if (frame.Hands.Count == 2)
-        {
-            Hand leftHand = frame.Hands[0];
-            Hand rightHand = frame.Hands[1];
+        Corps.GetComponent<Transform>().position = LeftPalm.GetComponent<Transform>().position;
+        Corps.GetComponent<Transform>().rotation = LeftPalm.GetComponent<Transform>().rotation;
+        Debug.Log(LeftPalm.GetComponent<Transform>().position.x);
 
-            if (leftHand.PalmPosition.x > rightHand.PalmPosition.x)
-            {
-                leftHand = rightHand;
-                rightHand = frame.Hands[0];
-                leftHand = frame.Hands[1];
-            }
-        }
-        if (frame.Hands.Count == 1)
-        {
-            Hand leftHand = frame.Hands[0];
-            Hand rightHand = frame.Hands[0];
-        }
-            if (frame.Hands.Count != 0)
-        {
+       
 
-            Vector3 Corpspos;
-            Corpspos.x = frame.Hands[0].PalmPosition.x / 100;
-            Corpspos.y = frame.Hands[0].PalmPosition.y / 100;
-            Corpspos.z = -frame.Hands[0].PalmPosition.z / 100;
-            Corps.transform.position = Corpspos;
-            Vector3 Corpsrot;
-            Corpsrot.x = frame.Hands[0].PalmNormal.x;
-            Corpsrot.y = frame.Hands[0].PalmNormal.y ;
-            Corpsrot.z = -frame.Hands[0].PalmNormal.z ;
-            Corps.transform.position = Corpsrot;
-
-
-
-        }
-        
 
     }
     
